254b7b44715a61671046d35970d1bbd661332b36
uma <uma2011ece@gmail.com>
2022-01-03 18:13:49 -0500

    Get the list of countries from endpoint instead of dummy data ,using Coroutine and Retrofit

--- app/src/main/java/com/devtides/androidcoroutinesretrofit/model/CountriesService.kt
+++ app/src/main/java/com/devtides/androidcoroutinesretrofit/model/CountriesService.kt
@@ -3,7 +3,9 @@ package com.devtides.androidcoroutinesretrofit.model
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class CountriesService {
+/*to make this class singleton ,we need to add object  keyword.so that only one instance used by whole application
+* if it is object class ,we no need to create new instance  every time when we need .we can directly access this class like static*/
+object CountriesService {
     private val BASE_URL="https://raw.githubusercontent.com"
 
     fun getCountriesService():CountriesApi{
--- app/src/main/java/com/devtides/androidcoroutinesretrofit/viewmodel/ListViewModel.kt
+++ app/src/main/java/com/devtides/androidcoroutinesretrofit/viewmodel/ListViewModel.kt
@@ -2,14 +2,23 @@ package com.devtides.androidcoroutinesretrofit.viewmodel
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.devtides.androidcoroutinesretrofit.model.CountriesService
 import com.devtides.androidcoroutinesretrofit.model.Country
+import kotlinx.coroutines.*
 import retrofit2.HttpException
+import kotlin.coroutines.coroutineContext
 
-class ListViewModel: ViewModel() {
+class ListViewModel : ViewModel() {
 
     val countries = MutableLiveData<List<Country>>()
     val countryLoadError = MutableLiveData<String?>()
     val loading = MutableLiveData<Boolean>()
+
+    var job: Job? = null
+    val countriesService = CountriesService.getCountriesService()
+    val exceptionHandler = CoroutineExceptionHandler { coroutineContext, throwable ->
+        onError("Exception:${throwable.localizedMessage}")
+    }
 
     fun refresh() {
         fetchCountries()
@@ -18,25 +27,42 @@ class ListViewModel: ViewModel() {
     private fun fetchCountries() {
         loading.value = true
 
-        val dummyData = generateDummyCountries()
+        /*here we are using retrofit to get response on background thread.
+        (for network communication we need to use IO dispatcher).
+        once the response available, we need  that result in the main thread to update UI*/
+     job= CoroutineScope(Dispatchers.IO+exceptionHandler).launch {
+            val response = countriesService.getCountries()
+
+            withContext(Dispatchers.Main) {
+                if (response.isSuccessful) {
+                    countries.value = response.body()
+                    countryLoadError.value = null
+                    loading.value = false
+                } else {
+                    onError("Error:${response.message()}")
+                }
+            }
+        }
+
+        /*val dummyData = generateDummyCountries()
 
         countries.value = dummyData
         countryLoadError.value = ""
-        loading.value = false
+        loading.value = false*/
     }
 
     private fun generateDummyCountries(): List<Country> {
         val countries = arrayListOf<Country>()
-        countries.add(Country("dummyCountry1",  "dummyCapital1",""))
-        countries.add(Country("dummyCountry2",  "dummyCapital2",""))
-        countries.add(Country("dummyCountry3",  "dummyCapital3",""))
-        countries.add(Country("dummyCountry4",  "dummyCapital4",""))
-        countries.add(Country("dummyCountry5",  "dummyCapital5",""))
-        countries.add(Country("dummyCountry1",  "dummyCapital1",""))
-        countries.add(Country("dummyCountry2",  "dummyCapital2",""))
-        countries.add(Country("dummyCountry3",  "dummyCapital3",""))
-        countries.add(Country("dummyCountry4",  "dummyCapital4",""))
-        countries.add(Country("dummyCountry5",  "dummyCapital5",""))
+        countries.add(Country("dummyCountry1", "dummyCapital1", ""))
+        countries.add(Country("dummyCountry2", "dummyCapital2", ""))
+        countries.add(Country("dummyCountry3", "dummyCapital3", ""))
+        countries.add(Country("dummyCountry4", "dummyCapital4", ""))
+        countries.add(Country("dummyCountry5", "dummyCapital5", ""))
+        countries.add(Country("dummyCountry1", "dummyCapital1", ""))
+        countries.add(Country("dummyCountry2", "dummyCapital2", ""))
+        countries.add(Country("dummyCountry3", "dummyCapital3", ""))
+        countries.add(Country("dummyCountry4", "dummyCapital4", ""))
+        countries.add(Country("dummyCountry5", "dummyCapital5", ""))
         return countries
     }
 
@@ -45,4 +71,9 @@ class ListViewModel: ViewModel() {
         loading.value = false
     }
 
+    /*when this view model is terminated we need to cancel the job */
+    override fun onCleared() {
+        super.onCleared()
+        job?.cancel()
+    }
 }
